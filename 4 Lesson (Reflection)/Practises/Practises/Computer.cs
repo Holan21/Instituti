cf766a7d5e343799d1ff5b8a39303b1d5ae46030
Holan <anton.kramarenko.holan@gmail.com>
2023-02-27 23:34:26 +0200

    Lesson 4(Reflection) Pratises

---
+++ 4 Lesson (Reflection)/Practises/Practises/Computer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practises
+{
+    public class Computer
+    {
+        public int CountProccesors;
+        internal int Ram;
+        public string Os;
+        internal double WAT;
+        public double Flops;
+
+        public void InitInfo()
+        {
+            CountProccesors = 10;
+            Ram = 8192;
+            Os = "Windows 11";
+            WAT = 921.45;
+            Flops = 1000.21;
+        }
+
+        internal string GetInfo()
+        {
+           return $"CountProccesors:{CountProccesors}\nRam:{Ram}\nOs:{Os}\nWAT:{WAT}\nFlops:{Flops}";
+        }
+
+        internal void ChangeOs(string Os)
+        {
+            this.Os = Os;
+        }
+    }
+}
---
+++ 4 Lesson (Reflection)/Practises/Practises/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+
+namespace Practises
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var computer = new Computer();
+            Type computerType = computer.GetType();
+            Console.WriteLine($"{computerType.FullName}\n");
+            TypeInfo computerTypeInfo = computerType.GetTypeInfo();
+            var fields = computerTypeInfo.GetFields();
+
+            for (int i = 0; i < fields.Length; i++)
+            {
+                Console.WriteLine(fields[i]);
+
+            }
+            Console.WriteLine();
+
+            var computerMemberInfo = computerType.GetMembers();
+
+            for (int i = 0; i < computerMemberInfo.Length; i++)
+            {
+                Console.WriteLine(computerMemberInfo[i]);
+            }
+
+            Console.WriteLine();
+
+            var computerFieldsInfo = computerType.GetFields();
+
+            for (int i = 0; i < computerFieldsInfo.Length; i++)
+            {
+                Console.WriteLine($"Type:{computerFieldsInfo[i]}\tAttributes:{computerFieldsInfo[i].Attributes}");
+            }
+
+            Console.WriteLine();
+
+            var computerMethodsInfo = computerType.GetMethods();
+
+            for (int i = 0; i < computerMethodsInfo.Length; i++)
+            {
+                Console.WriteLine($"{computerMethodsInfo[i]}");
+            }
+
+            computerMethodsInfo[0].Invoke(computer , null);
+        }
+    }
+}
